8a7cbd04aad016036b39cf4fe56850101101fe49
Tim Kravel <TKravel@Yahoo.com>
2022-04-17 21:52:00 -0400

    remove commented out code for images

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,33 +1,12 @@
-import { useEffect, useState } from 'react';
-
 export const Image = ({ data, styles }) => {
-	const [isLoading, setIsLoading] = useState(true);
-
-	setTimeout(() => {
-		setIsLoading(false);
-	}, 400);
-	// const handleImage = () => {
-	// 	setIsLoading(false);
-	// };
 	return (
 		<div className='image-wrapper'>
-			<div
-				className='img-placeholder'
-				style={isLoading ? { display: 'block' } : { display: 'none' }}
-			></div>
-
 			<img
-				style={
-					isLoading
-						? { visibility: 'hidden' }
-						: { visibility: 'visible' }
-				}
 				className={styles}
 				height='206'
 				width='365'
 				src={data.thumbnail}
 				alt={`${data.title} cover`}
-				// onLoad={handleImage}
 				sameSite='Strict'
 			></img>
 		</div>
